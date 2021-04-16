96d999276e430b208661d8d88c3a3379920d7938
Francine Guimarães <francinesguimaraes@outlook.com>
2021-04-16 11:40:14 -0300

    chore: refactor accounts controller

--- apps/banking_api_web/lib/banking_api_web/controllers/accounts_controller.ex
+++ apps/banking_api_web/lib/banking_api_web/controllers/accounts_controller.ex
@@ -2,22 +2,23 @@ defmodule BankingApiWeb.Controllers.AccountsController do
   use BankingApiWeb, :controller
 
   alias BankingApiWeb.Views.AccountsView
+  alias BankingApiWeb.Views.ErrorView
+  alias Ecto.Changeset
 
   alias BankingApi.Accounts
   def create(conn, params) do
-    with {:ok, %Accounts{} = account} <- BankingApi.create_account(params) do
+    case BankingApi.create_account(params) do
+    {:error,  %Changeset{} = changeset} ->
+      conn
+      |> put_view(ErrorView)
+      |> IO.inspect(label: "Create account")
+      |> put_status(:bad_request)
+      |> render("400.json", result: changeset)
+    {:ok, %Accounts{} = account} ->
       conn
       |> put_view(AccountsView)
-      |> IO.inspect(label: "Create account")
       |> put_status(:created)
       |> render("create.json", account: account)
     end
   end
-
-  # defp handle_response({:ok, %Accounts{} = account}, conn) do
-  #   conn
-  #   |> put_view(AccountsView)
-  #   |> put_status(:created)
-  #   |> render("create.json", account: account)
-  # end
 end
